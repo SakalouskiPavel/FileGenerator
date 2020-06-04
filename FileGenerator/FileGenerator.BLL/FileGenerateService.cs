b8f8e29f43dbaad5a748a13da344faf1fb10d148
Pavel_Sakalouski <p.sokolovsky62@gmail.com>
2020-06-04 22:45:01 +0300

    Generate default number of files with default content (hardcoded)

---
+++ FileGenerator/FileGenerator.BLL/FileGenerateService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using FileGenerator.BLL.Properties;
+
+namespace FileGenerator.BLL
+{
+    public class FileGenerateService
+    {
+        private const string defaultNamePrefix = "TestFile";
+
+        private const int charSize = 3;
+
+        private const int defaultFileSizeKB = 150;
+
+        private const int kiloByteSize = 1024;
+
+        public void GenerateFiles()
+        {
+            for (int i = 0; i < 400; i++)
+            {
+                string path = @"D:\testFolder\" + defaultNamePrefix + i + ".txt";
+                using (StreamWriter sw = new StreamWriter(new FileStream(path, FileMode.OpenOrCreate)))
+                {
+                    for (int j = 0; j < (defaultFileSizeKB * kiloByteSize) / charSize; j++)
+                    {
+                        sw.WriteLine(Resources.defaultFileContent);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ FileGenerator/FileGenerator.Console/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FileGenerator.BLL;
+
+namespace FileGenerator.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var service = new FileGenerateService();
+            service.GenerateFiles();
+
+            System.Console.ReadKey();
+        }
+    }
+}
